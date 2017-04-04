dacfc0fe3165aa84074123451ed69299e51e8fbf
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-04 10:05:46 +0200

    delete obsolete function: convert_format

--- R/date_convert.R
+++ R/date_convert.R
@@ -1,43 +1,3 @@
-#' Convert the format of a POSIXct date
-#'
-#' Convert the format of a POSIXct date
-#' SOON OBSOLETE
-#' @param date format POSIXct
-#' @param formatTo conversion into POSIXct, into a vector "YYMMDDHH" or a matrix "YY,MM,DD,HH"
-#' @keywords timeManip
-#' @export
-#' @examples
-#' res <- convertFormat(strptime("2016032701","%Y%m%d%H%",tz="GMT"),"YY,MM,DD,HH")
-#' res <- convertFormat(strptime("2016032701","%Y%m%d%H%",tz="GMT"),"YYMMDDHH")
-
-convertFormat <- function(date,formatTo){
-
-  res <- switch(formatTo,
-          "POSIXct"        = date,
-          "YYMMDDHH"       = vectorFormat(date=date),
-          "YY,MM,DD,HH"    = matrixFormat(date=date),
-          (message=paste0("Invalid format expression:", formatTo,". Format conversion not yet coded"))
-  )
-
-   return(res)
-}
-
-vectorFormat <- function(date){
-  res <- gsub("GMT","",gsub(":","",gsub("-","",date)))
-  return(res)
-}
-
-matrixFormat <- function(date){
-  datetmp <- base::as.POSIXlt(date)
-  tmp <- list(year=(1900+datetmp$year),
-              month=(1+datetmp$mon),
-              day=datetmp$mday,
-              hour=datetmp$hour)
-  res <- cbind(tmp$year,tmp$month,tmp$day,tmp$hour)
-  return(res)
-}
-
-
 
 #' Get YYYY
 #'
@@ -119,6 +79,18 @@ YYYYmmddHHMMSS <- function(date){
 
 
 
+#' Get YYYY_m
+#'
+#' Get the year as a matrix
+#' @param date POSIXlt
+#' @keywords timeManip
+#' @export
+#' @examples
+#' res <- YYYYmmddHH_chr("2015010100")
+#' YYYY_m(res)
+YYYY_m <- function(date){
+  return(cbind(year(date)))
+}
 
 #' Get YYYYmm_m
 #'
@@ -182,5 +154,45 @@ YYYYmmddHHMM_m <- function(date){
 #' res <- YYYYmmddHHMMSS_chr("20150101001034")
 #' YYYYmmddHHMMSS_m(res)
 YYYYmmddHHMMSS_m <- function(date){
-  return(cbind(year(date,CHAR=TRUE),month(date,CHAR=TRUE),day(date,CHAR=TRUE),hour(date,CHAR=TRUE),minute(date,CHAR=TRUE),second(date,CHAR=TRUE)))
+  return(cbind(year(date),month(date),day(date),hour(date),minute(date),second(date)))
+}
+
+
+#' Convert the format of a POSIXct date
+#'
+#' Convert the format of a POSIXct date
+#' SOON OBSOLETE
+#' @param date format POSIXct
+#' @param formatTo conversion into POSIXct, into a vector "YYMMDDHH" or a matrix "YY,MM,DD,HH"
+#' @keywords timeManip
+#' @export
+#' @examples
+#' res <- convertFormat(strptime("2016032701","%Y%m%d%H%",tz="GMT"),"YY,MM,DD,HH")
+#' res <- convertFormat(strptime("2016032701","%Y%m%d%H%",tz="GMT"),"YYMMDDHH")
+
+convertFormat <- function(date,formatTo){
+
+  res <- switch(formatTo,
+          "POSIXct"        = date,
+          "YYMMDDHH"       = vectorFormat(date=date),
+          "YY,MM,DD,HH"    = matrixFormat(date=date),
+          (message=paste0("Invalid format expression:", formatTo,". Format conversion not yet coded"))
+  )
+
+   return(res)
+}
+
+vectorFormat <- function(date){
+  res <- gsub("GMT","",gsub(":","",gsub("-","",date)))
+  return(res)
+}
+
+matrixFormat <- function(date){
+  datetmp <- base::as.POSIXlt(date)
+  tmp <- list(year=(1900+datetmp$year),
+              month=(1+datetmp$mon),
+              day=datetmp$mday,
+              hour=datetmp$hour)
+  res <- cbind(tmp$year,tmp$month,tmp$day,tmp$hour)
+  return(res)
 }
