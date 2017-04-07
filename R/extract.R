b9b09b5575a3ef0cb4e5b3a5defbe1db0088af8c
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-07 16:23:38 +0200

    function extract

--- R/extract.R
+++ R/extract.R
@@ -9,12 +9,13 @@
 #' @examples
 #' a <- timeManip(fromPeriod="2013060205",toPeriod="2013062805",
 #'                timeResolution="hourly",precision="hourly")
-#' res <- extract(a,c(1,5,8))
-#' str(res)
-extract <- function(date, indice=NULL){
+#' res <- extract(a,indice=list(offset=3,count=5))
+#' res$fromPeriod()
+#' res$toPeriod()
+extract <- function(date, indice=NULL){ #list(offset,count)
   if (!is.null(indice)) {
-     res <- timeManip(fromPeriod     = YYYYmmddHHMMSS_chr(date$seqPeriod(indice[1])),
-                      toPeriod       = YYYYmmddHHMMSS_chr(date$seqPeriod(indice[2])),
+     res <- timeManip(fromPeriod     = YYYYmmddHHMMSS(date$seqPeriod(indice$offset)),
+                      toPeriod       = YYYYmmddHHMMSS(date$seqPeriod((indice$offset+indice$count-1))),
                       timeResolution = date$timeResolution(),
                       precision      = date$precision())
   } else res <- date
