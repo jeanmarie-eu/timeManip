9b504f8157be02393595a85b1c01b417491d1858
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-20 15:48:11 +0200

    make arguments name understandable

--- R/contain.R
+++ R/contain.R
@@ -2,17 +2,20 @@
 #'
 #' contain
 #'
-#' @param a POSIXlt timeserie
-#' @param b POSIXlt timeserie
+#' @param ts POSIXlt timeserie
+#' @param ts_sub POSIXlt sub timeserie
 #' @keywords timeManip
 #' @export
 #' @examples
 #' \dontrun{
 #' contain()
 #' }
-contain  <- function(a,b){
-  i1 <- which(b>=a[1])
-  i2 <- which(b<=a[length(a)])
-  indice <- seq(i1[1],i2[length(i2)])
+contain  <- function(ts,ts_sub){
+  i1 <- i2 <- NULL
+  i1 <- which(ts_sub>=ts[1])
+  i2 <- which(ts_sub<=ts[length(ts)])
+  if (!is.null(i1) && !is.null(i2)) {
+     indice <- seq(i1[1],i2[length(i2)])
+  } else indice <- NULL
   return(indice)
 }
