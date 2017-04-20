17dd3b0b114223e65e5ac95df0b8465854499e61
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-20 15:49:51 +0200

    add possibility of n*timeresolution

--- R/date_timeserie.R
+++ R/date_timeserie.R
@@ -2,6 +2,7 @@
 #'
 #' The fuction \code{dateTimeSerie()} builds the timeserie of date
 #' @param timeResolution "daily","hourly","minute","second"
+#' @param v multiplicative coefficient to the timeResolution
 #' @param fromPeriod date char "YYYYmmddH..."
 #' @param toPeriod date char "YYYYmmdd..."
 #' @param precision "daily","hourly","minute","second",...
@@ -13,7 +14,9 @@
 #' head(res)
 #' res <- timeserie("daily","1980022802","1980030102","hourly")
 #' head(res)
-timeserie <- function(timeResolution,fromPeriod,toPeriod,precision=NULL){
+#' res <- timeserie("hourly",v=2,"1980022802","1980030102","hourly")
+#' head(res)
+timeserie <- function(timeResolution,fromPeriod,toPeriod,precision=NULL,v=1){
 
   if (is.null(precision) && (timeResolution!="three-hourly")) {
     precision <- timeResolution
@@ -26,11 +29,11 @@ timeserie <- function(timeResolution,fromPeriod,toPeriod,precision=NULL){
   seqPeriod <- switch(timeResolution,
     "yearly"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = "year"),
     "monthly"      = seq(from = from_POSIXlt, to = to_POSIXlt, by = "month"),
-    "daily"        = seq(from = from_POSIXlt, to = to_POSIXlt, by = (24*3600)),
-    "three-hourly" = seq(from = from_POSIXlt, to = to_POSIXlt, by = (3*3600)),
-    "hourly"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (3600)),
-    "minute"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (60)),
-    "second"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (1)),
+    "daily"        = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*24*3600)),
+    "three-hourly" = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*3600)),
+    "hourly"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*3600)),
+    "minute"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*60)),
+    "second"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*1)),
     (message=paste0("Invalid time resolution:", timeResolution,"."))
   )
   nbStep <- length(seqPeriod)
