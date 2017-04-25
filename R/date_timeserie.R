d5a94086f0f51f4d84d80c4673135d94a001b5bf
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-25 11:26:16 +0200

    typo correction for three-hourly time resolution

--- R/date_timeserie.R
+++ R/date_timeserie.R
@@ -20,11 +20,10 @@ timeserie <- function(timeResolution,fromPeriod,toPeriod,precision=NULL,v=1){
 
   if (is.null(precision) && (timeResolution!="three-hourly")) {
     precision <- timeResolution
-    v <- 3
   } else if(is.null(precision) && (timeResolution=="three-hourly")) {
     precision <- "hourly"
-    v <- 3
   }
+  
   from_POSIXlt <- standard(precision,fromPeriod)
   to_POSIXlt   <- standard(precision,toPeriod)
 
@@ -32,7 +31,7 @@ timeserie <- function(timeResolution,fromPeriod,toPeriod,precision=NULL,v=1){
     "yearly"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = "year"),
     "monthly"      = seq(from = from_POSIXlt, to = to_POSIXlt, by = "month"),
     "daily"        = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*24*3600)),
-    "three-hourly" = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*3600)),
+    "three-hourly" = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*3*3600)),
     "hourly"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*3600)),
     "minute"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*60)),
     "second"       = seq(from = from_POSIXlt, to = to_POSIXlt, by = (v*1)),
