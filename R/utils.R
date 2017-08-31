1e4e898c2f4fdd63310f6d81af0740daa2cdbf56
jeanmarie-eu <jeanmarie-eu@users.noreply.github.com>
2017-08-31 10:36:04 +0200

    import str and etFromNamespace

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,10 @@
+#' @importFrom utils getFromNamespace
+utils::getFromNamespace
+
+#' @importFrom utils str
+utils::str
+
 #' @export
 print.timeManip <- function(x,...){
-   str(x$summary())  
+   str(x$summary())
 }
